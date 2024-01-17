4a8f88ecd74c6c395c0fa94f63bf6d04fcab94e9
Krzysztof Kosowski <dev.kosowski76@gmail.com>
2024-01-17 05:36:41 +0100

    designing/Fibonacci-sequence-01# 02.01. Designing algorithms and tests.

--- src/tests/sum_mechanics/sum_mechanics_test.go
+++ src/tests/sum_mechanics/sum_mechanics_test.go
@@ -1,9 +1,13 @@
 package sum_mechanics_test
 
-import "github.com/kosowski76/golang_fibonacci_sequence_tdd/src/domain/fibonacci_sequence"
+import (
+	"github.com/kosowski76/golang_fibonacci_sequence_tdd/src/domain/fibonacci_sequence"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 // todo: test if another negative number works
-func GivenNegativeHourWhenCountingFiboShouldTrowNFE() {
+func TestGivenNegativeHourWhenCountingFiboShouldTrowNFE(t *testing.T) {
 
 	number := -1
 	// calculateFor(number)
@@ -11,15 +15,67 @@ func GivenNegativeHourWhenCountingFiboShouldTrowNFE() {
 	// todo: should throw NumberFormatException
 }
 
-func Given0shouldReturn0() {
+func TestGiven0shouldReturn0(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(0)
+	assert.EqualValues(t, 0, fibonacciCalc)
 }
-func Given1shouldReturn1() {
+func TestGiven1shouldReturn1(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(1)
+	assert.EqualValues(t, 1, fibonacciCalc)
 }
-func Given2shouldReturn1() {
+func TestGiven2shouldReturn1(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(2)
+	assert.EqualValues(t, 1, fibonacciCalc)
 }
-func Given3shouldReturn2() {
+func TestGiven3shouldReturn2(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(3)
+	assert.EqualValues(t, 2, fibonacciCalc)
 }
-func Given10shouldReturn54() {
+func TestGiven10shouldReturn54(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(10)
+	assert.EqualValues(t, 54, fibonacciCalc)
 }
-func Given9shouldReturn34() {
+func TestGiven9shouldReturn34(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(9)
+	assert.EqualValues(t, 34, fibonacciCalc)
+}
+func TestGiven23shouldReturn28657(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(23)
+	assert.EqualValues(t, 28657, fibonacciCalc)
+}
+func TestGiven24shouldReturn46368(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(24)
+	assert.EqualValues(t, 46368, fibonacciCalc)
+}
+func TestGiven25shouldReturn75025(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(25)
+	assert.EqualValues(t, 75025, fibonacciCalc)
+}
+func TestGiven27shouldReturn196418(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(27)
+	assert.EqualValues(t, 196418, fibonacciCalc)
+}
+func TestGiven28shouldReturn317811(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(28)
+	assert.EqualValues(t, 317811, fibonacciCalc)
+}
+func TestGiven29shouldReturn514229(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(29)
+	assert.EqualValues(t, 514229, fibonacciCalc)
+}
+func TestGiven30shouldReturn832040(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(30)
+	assert.EqualValues(t, 832040, fibonacciCalc)
+}
+func TestGiven31shouldReturn1346269(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(31)
+	assert.EqualValues(t, 1346269, fibonacciCalc)
+}
+func TestGiven47shouldReturn2971215073(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(47)
+	assert.EqualValues(t, 2971215073, fibonacciCalc)
+}
+func TestGivenNegative1shouldReturnException(t *testing.T) {
+	fibonacciCalc := fibonacci_sequence.FibonacciSequence(-1)
+	assert.EqualValues(t, 0000, fibonacciCalc)
 }
